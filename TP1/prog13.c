51052015c18ee86c8e7b3a10d39031e50c1de483
Mavelote <silver.superhomem@gmail.com>
2026-03-16 13:10:17 +0000

    Remove splitFields function implementation from prog13.c

--- TP1/prog13.c
+++ TP1/prog13.c
@@ -21,28 +21,3 @@ int main(){
     return 0;
     
 }
-
-int splitFields(char *str, char *fields[], int max){
-    int count = 0;
-
-    if (max > 0)
-    {
-        fields[count] = str;
-    }
-    count++;
-    
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == ';')
-        {
-            str[i] = '\0';
-            if (count < max)
-            {
-                fields[count] = &str[i+1];
-            }
-            count++;
-        }
-        
-    }
-    return count;
-}
